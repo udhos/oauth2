56ab8457708b2d646be4f96b302ce67506ad6355
udhos <udhos>
2023-12-15 17:49:17 -0300

    Simple client credential test.

--- clientcredentials/clientcredentials_test.go
+++ clientcredentials/clientcredentials_test.go
@@ -1,6 +1,12 @@
 package clientcredentials
 
 import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 )
@@ -52,4 +58,147 @@ func TestParseToken(t *testing.T) {
 			t.Logf("%s: ok", data.name)
 		}
 	}
+}
+
+func TestClientCredentials(t *testing.T) {
+
+	clientID := "clientID"
+	clientSecret := "clientSecret"
+	token := "abc"
+
+	tokenServerStat := serverStat{}
+	serverStat := serverStat{}
+
+	ts := newTokenServer(&tokenServerStat, clientID, clientSecret, token)
+	defer ts.Close()
+
+	srv := newServer(&serverStat, token)
+	defer srv.Close()
+
+	client := newClient(ts.URL, clientID, clientSecret)
+
+	// send 1
+
+	{
+		_, errSend := send(client, srv.URL)
+		if errSend != nil {
+			t.Errorf("send: %v", errSend)
+		}
+		if tokenServerStat.count != 1 {
+			t.Errorf("unexpected token server access count: %d", tokenServerStat.count)
+		}
+		if serverStat.count != 1 {
+			t.Errorf("unexpected server access count: %d", serverStat.count)
+		}
+	}
+
+	// send 2
+
+	_, errSend2 := send(client, srv.URL)
+	if errSend2 != nil {
+		t.Errorf("send: %v", errSend2)
+	}
+	if tokenServerStat.count != 1 {
+		t.Errorf("unexpected token server access count: %d", tokenServerStat.count)
+	}
+	if serverStat.count != 2 {
+		t.Errorf("unexpected server access count: %d", serverStat.count)
+	}
+}
+
+func send(client *Client, serverURL string) (string, error) {
+
+	req, errReq := http.NewRequestWithContext(context.TODO(), "GET", serverURL, nil)
+	if errReq != nil {
+		return "", fmt.Errorf("request: %v", errReq)
+	}
+
+	resp, errDo := client.Do(req)
+	if errDo != nil {
+		return "", fmt.Errorf("do: %v", errDo)
+	}
+	defer resp.Body.Close()
+
+	body, errBody := io.ReadAll(resp.Body)
+	if errBody != nil {
+		return "", fmt.Errorf("body: %v", errBody)
+	}
+
+	bodyStr := string(body)
+
+	if resp.StatusCode != 200 {
+		return "", fmt.Errorf("bad status:%d body:%v", resp.StatusCode, bodyStr)
+	}
+
+	return bodyStr, nil
+}
+
+func formParam(r *http.Request, key string) string {
+	v := r.Form[key]
+	if v == nil {
+		return ""
+	}
+	return v[0]
+}
+
+func newServer(stat *serverStat, token string) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		stat.count++
+		h := r.Header.Get("Authorization")
+		t := strings.TrimPrefix(h, "Bearer ")
+		if t != token {
+			httpJSON(w, `{"error":"unauthorized"}`, http.StatusUnauthorized)
+			return
+		}
+		httpJSON(w, `{"message":"ok"}`, http.StatusOK)
+	}))
+}
+
+// httpJSON replies to the request with the specified error message and HTTP code.
+// It does not otherwise end the request; the caller should ensure no further
+// writes are done to w.
+// The message should be JSON.
+func httpJSON(w http.ResponseWriter, message string, code int) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(code)
+	fmt.Fprintln(w, message)
+}
+
+type serverStat struct {
+	count int
+}
+
+func newTokenServer(serverInfo *serverStat, clientID, clientSecret, token string) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		serverInfo.count++
+
+		r.ParseForm()
+		formGrantType := formParam(r, "grant_type")
+		formClientID := formParam(r, "client_id")
+		formClientSecret := formParam(r, "client_secret")
+
+		if formGrantType != "client_credentials" || formClientID != clientID || formClientSecret != clientSecret {
+			httpJSON(w, `{"error":"unauthorized"}`, http.StatusUnauthorized)
+			return
+		}
+
+		t := fmt.Sprintf(`{"access_token":"%s"}`, token)
+
+		httpJSON(w, t, http.StatusOK)
+	}))
+}
+
+func newClient(tokenURL, clientID, clientSecret string) *Client {
+	options := Options{
+		TokenURL:     tokenURL,
+		ClientID:     clientID,
+		ClientSecret: clientSecret,
+		HTTPClient:   http.DefaultClient,
+	}
+
+	client := New(options)
+
+	return client
 }
